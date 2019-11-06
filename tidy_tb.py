eedcf09a274c229ca3dc72f5c329a7c22dd56856
Kimmirikwa <kimrodrikwa@gmail.com>
2019-11-06 16:07:18 +0300

    melt tb data

--- tidy_tb.py
+++ tidy_tb.py
@@ -10,3 +10,9 @@ tb_df = pd.read_csv('data/tb.csv')
 # 'new_sp_f4554', 'new_sp_f5564', 'new_sp_f65', 'new_sp_fu'
 # m -> male, f -> female, 514 -> 5 - 14
 print("The original columns", tb_df.columns)
+
+# we will first melt this data to convert all other columns except 'iso2', 'year', and 'new_sp' to 2 columns
+# next will extract sex and age from the created variable column to and add the values to respective columns
+molten_tb_df = pd.melt(tb_df, id_vars=['iso2', 'year', 'new_sp'], var_name='sex-age', value_name='count')
+# The new columns are 'iso2', 'year', 'new_sp', 'age-sex', 'count'
+print("The columns for molten data", molten_tb_df.columns)
