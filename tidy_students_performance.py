c05dc302ec44b8c65bc505384789bc09f66cfd34
Kimmirikwa <kimrodrikwa@gmail.com>
2019-11-09 09:23:41 +0300

    extract sex and age

--- tidy_students_performance.py
+++ tidy_students_performance.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+def get_sex_and_age(row):
+	sex_and_age = row['sex and age'].split('_')
+	sex = sex_and_age[0]
+	age = sex_and_age[1]
+
+	return pd.Series([sex, age])
+
 students_performance_df = pd.read_csv('data/students_performance.csv')
 
 # the columns of the dataframe
@@ -10,3 +17,8 @@ print("The columns of the original dataframe", students_performance_df.columns.t
 # the 2 dataset will have 'id' column, which should be common between them
 student_df = students_performance_df[['id', 'name', 'phone', 'sex and age']]
 performance_df = students_performance_df[['id', 'test number', 'term 1', 'term 2', 'term 3']]
+
+# for student_df, the 'sex and age' column needs to be split into 2
+student_df[['sex', 'age']] = student_df.apply(lambda row: get_sex_and_age(row), axis=1)
+# dropping the 'sex and age' column
+student_df.drop(['sex and age'], axis=1, inplace=True)
