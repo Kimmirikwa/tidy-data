85bac128d30dcfa6ffa70bca427abe45a4e21e08
Kimmirikwa <kimrodrikwa@gmail.com>
2019-11-09 10:09:37 +0300

    marks for each test

--- tidy_students_performance.py
+++ tidy_students_performance.py
@@ -9,6 +9,20 @@ def get_sex_and_age(row):
 
 	return pd.Series([sex, age])
 
+def get_term_no(row):
+	return row['term'][-1]
+
+def get_marks_for_tests(dataset, row):
+	student_id = row['id']
+	term = row['term']
+	test1_observation = dataset.loc[(dataset['id'] == student_id) & (dataset['term'] == term) & (dataset['test number'] == 'test 1')].iloc[0]
+	test2_observation = dataset.loc[(dataset['id'] == student_id) & (dataset['term'] == term) & (dataset['test number'] == 'test 2')].iloc[0]
+
+	test1_marks = test1_observation['marks']
+	test2_marks = test2_observation['marks']
+
+	return pd.Series([test1_marks, test2_marks])
+
 students_performance_df = pd.read_csv('data/students_performance.csv')
 
 # the columns of the dataframe
@@ -33,3 +47,11 @@ student_df.to_csv('results/tidy_students.csv', index=False)
 # now dealing with the performance data
 # we first melt the data to have 'term 1', 'term 2' and 'term 3' in their own columns
 molten_performance_df = pd.melt(performance_df, id_vars=['id', 'test number'], var_name='term', value_name='marks')
+
+# extracting term number
+molten_performance_df['term'] = molten_performance_df.apply(lambda row: get_term_no(row), axis=1)
+
+# marks for each term
+molten_performance_df[['test 1 marks', 'test 2 marks']] = molten_performance_df.apply(
+	lambda row: get_marks_for_tests(molten_performance_df, row), axis=1)
+
